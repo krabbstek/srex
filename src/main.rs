d9362b97daafc9f3fade5772f00ca4a696150b32
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-19 19:37:26 +0100

    Added shell CLI code

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Create,
+    Merge {
+        #[arg(short = 'o', long = "output", default_value = None)]
+        output: Option<std::path::PathBuf>,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+    println!("{:?}", args.cmd);
+}
