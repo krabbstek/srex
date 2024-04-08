851084f57031c643dbe24e776f1ee56db97734f7
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-04-08 22:46:40 +0200

    Added code for benching from_str

--- benches/srecord.rs
+++ benches/srecord.rs
@@ -1,6 +1,8 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::str::FromStr;
 
-use srex::srecord::calculate_checksum;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use srex::srecord::*;
 
 fn bench_calculate_checksum(c: &mut Criterion) {
     let mut group = c.benchmark_group("calculate_checksum");
@@ -14,5 +16,25 @@ fn bench_calculate_checksum(c: &mut Criterion) {
     });
 }
 
-criterion_group!(calculate_checksum_slkjkldsf, bench_calculate_checksum,);
-criterion_main!(calculate_checksum_slkjkldsf);
+fn bench_from_str(c: &mut Criterion) {
+    let mut srecord_str = String::new();
+    let num_rows = 1000000;
+    srecord_str.reserve("S113000000000000000000000000000000000000EC".len() * num_rows);
+    for i in 0..num_rows {
+        let address = i * 16;
+        srecord_str
+            .push_str(format!("S113{address:04X}00000000000000000000000000000000EC").as_str());
+    }
+
+    let mut group = c.benchmark_group("from_str");
+    group.bench_with_input("from_str", srecord_str.as_str(), |b, s| {
+        b.iter(|| SRecordFile::from_str(s));
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = bench_calculate_checksum, bench_from_str
+}
+criterion_main!(benches);
