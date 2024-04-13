c35a34407f82e278efb862496272f4ca73ebf409
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-04-13 10:43:35 +0200

    Added RecordType documentation

--- src/srecord.rs
+++ src/srecord.rs
@@ -7,22 +7,52 @@ use std::str::FromStr;
 
 use hex;
 
+/// Enum containing which type a [`Record`] is.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum RecordType {
-    S0, // Header
-    S1, // 16-bit address data
-    S2, // 24-bit address data
-    S3, // 32-bit address data
+    /// Header record. 16-bit address that must be 0x0000.
+    S0,
+    /// 16-bit address data record.
+    S1,
+    /// 24-bit address data record.
+    S2,
+    /// 32-bit address data record.
+    S3,
     // S4 is reserved
-    S5, // 16-bit count
-    S6, // 24-bit count
-    S7, // 32-bit start address
-    S8, // 24-bit start address
-    S9, // 16-bit start address
+    /// 16-bit record count, containing the count of records that have occurred in the SRecord file.
+    /// Can only occur once in an SRecord file and must be put last, after all the data records but
+    /// before the start address record. Should be used instead of [`S6`](RecordType::S6) if the
+    /// record count is less than 65536 (0x10000).
+    S5,
+    /// 24-bit record count, containing the count of records that have occurred in the SRecord file.
+    /// Can only occur once in an SRecord file and must be put last, after all the data records but
+    /// before the start address record. Should be used instead of [`S5`](RecordType::S5) if the
+    /// record count is equal to or greater than 65536 (0x10000), otherwise [`S5`](RecordType::S5)
+    /// shall be used instead.
+    S6,
+    /// 32-bit address containing the start execution location. This is an optional record, but when
+    /// it occurs it must be the very last record of the file.
+    S7,
+    /// 24-bit address containing the start execution location. This is an optional record, but when
+    /// it occurs it must be the very last record of the file.
+    S8,
+    /// 16-bit address containing the start execution location. This is an optional record, but when
+    /// it occurs it must be the very last record of the file.
+    S9,
 }
 
 impl RecordType {
-    fn num_address_bytes(&self) -> usize {
+    /// Returns the number of address bytes a certain record type contains.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use srex::srecord::RecordType;
+    ///
+    /// let record_type = RecordType::S2;
+    /// assert_eq!(record_type.num_address_bytes(), 3);
+    /// ```
+    pub fn num_address_bytes(&self) -> usize {
         match *self {
             RecordType::S0 => 2,
             RecordType::S1 => 2,
@@ -38,6 +68,16 @@ impl RecordType {
 }
 
 impl fmt::Display for RecordType {
+    /// Formats the record type including a leading S.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use srex::srecord::RecordType;
+    ///
+    /// let record_type = RecordType::S5;
+    /// assert_eq!(format!("{record_type}"), "S5");
+    /// ```
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             RecordType::S0 => write!(f, "S0"),
